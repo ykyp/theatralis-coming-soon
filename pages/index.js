eb9db8b4ee3fb2b4adc0418b671879056a56e2a6
ykyp <yannakyp@hotmail.com>
2021-05-17 22:39:10 +0300

    comiing soon only

--- pages/index.js
+++ pages/index.js
@@ -10,114 +10,15 @@ import { useState, useEffect } from 'react';
 import { Paginator } from 'primereact/paginator';
 import * as ga from '../lib/ga'
 
-export default function Home({ allEventsData }) {
-   const [results, setResults] = useState([]);
-   const [selectedCity, setSelectedCity] = useState({ name: 'Anywhere', code: 'ALL' });
-   const [selectedPeriod, setSelectedPeriod] = useState({ name: 'Anytime', code: 'ALL' });
-   const [selectedAudience, setSelectedAudience] = useState({ name: 'Everyone', code: 'ALL' });
-   const [pageFirst, setPageFirst] = useState(0);
-   const [currentPage, setCurrentPage] = useState(0);
-   const [rowsPerPage, setRowsPerPage] = useState(10);
-   const [currentTotalCount, setCurrentTotalCount] = useState(allEventsData.length);
-
-   const searchEndpoint = (city, period, audience, page, rows) => `/api/search?city=${city}&period=${period}&audience=${audience}&page=${page}&rows=${rows}`;
-
-   const handleCityChange = (e) => {
-      setSelectedCity(e.value);
-   };
-
-   const handlePeriodChange = (e) => {
-      setSelectedPeriod(e.value);
-   };
-
-   const handleAudienceChange = (e) => {
-      setSelectedAudience(e.value);
-   };
-
-   const onBasicPageChange = (event) => {
-      setCurrentPage(event.page); 
-      setPageFirst(event.first); 
-      setRowsPerPage(event.rows);
-   };
-
-   useEffect(() => {
-      searchEvents();
-   }, [selectedPeriod, selectedCity, selectedAudience, currentPage]);
-
-   const searchEvents = () => {
-      const query = {
-         city:  selectedCity.code === 'ALL'? 'ALL' : selectedCity.name,
-         period: selectedPeriod.code,
-         audience:  selectedAudience.code === 'ALL'? 'ALL' : selectedAudience.name,
-         page: currentPage,
-         rows: rowsPerPage,
-      };
-      fetch(searchEndpoint(query.city,
-         query.period,
-         query.audience,
-         query.page,
-         query.rows))
-         .then(res => res.json())
-         .then(res => {
-            setResults(res.results);
-            setCurrentTotalCount(res.totalLength);
-         });
-
-      ga.event({
-         action: "search",
-         params : {
-            search_term: selectedCity.code
-         }
-      })
-   };
-
+export default function Home() {
    return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.textAlignCenter}`}>
-        <h2>Find all theatre events in Cyprus</h2>
+        <h2>Coming soon!</h2>
       </section>
-
-       <Filter onCityChange={handleCityChange}
-               selectedCity={selectedCity}
-               onPeriodChange={handlePeriodChange}
-               selectedPeriod={selectedPeriod}
-               onAudienceChange={handleAudienceChange}
-               selectedAudience={selectedAudience}
-       />
-
-
-       {/*<h2 className={utilStyles.headingLg}>Search</h2>
-       <Search />*/}
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Events {selectedCity && `in ${selectedCity.name}`}</h2>
-         { results.length === 0 && <div>No Events found.</div> }
-        <div className={utilStyles.list}>
-          {results.map((event) => (
-             <ListingEvent event={event} />
-           /* <li className={utilStyles.listItem} key={id}>
-              <Link href={`/events/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                 {city}, {/!*<FormattedDate dateString={startDate} /> - <FormattedDate dateString={endDate} />*!/}
-              </small>
-               { event_image && <img width={35} height={35} src={event_image}/> }
-            </li>*/
-          ))}
-        </div>
-      </section>
-
-       <Paginator first={pageFirst}
-                  rows={rowsPerPage}
-                  totalRecords={currentTotalCount}
-                  rowsPerPageOptions={[10, 20, 30]}
-                  onPageChange={onBasicPageChange}>
-       </Paginator>
     </Layout>
   )
 }
