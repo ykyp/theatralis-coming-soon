bebcec6a9060f58a112694c2fabb411e6843df80
ykyp <yannakyp@hotmail.com>
2021-05-17 22:47:11 +0300

    new img

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/thoc.jpg"
+              src="/images/theatralis.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -42,7 +42,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/theatralis.jpeg"
+                  src="/images/theatralis.png"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedEventsData } from '../lib/events'
-import Link from 'next/link'
-import FormattedDate from '../components/date'
-import { Filter } from '../components/filter';
-import { ListingEvent } from '../components/listing-event';
-import { useState, useEffect } from 'react';
-import { Paginator } from 'primereact/paginator';
-import * as ga from '../lib/ga'
 
 export default function Home() {
    return (
